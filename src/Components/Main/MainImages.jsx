f3ccb31b6b9af7ab6346bc735aa4a8eff82c028b
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-04-26 14:51:37 +0500

    AI-Automation

---
+++ src/Components/Main/MainImages.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const MainImages = () => {
+  return (
+    <div>
+      <div className="right">
+        <div className="image1"></div>
+        <div className="image2"></div>
+      </div>
+      <div className="center">
+        <div className="image3"></div>
+        <div className="image4"></div>
+      </div>
+      <div className="left">
+        <div className="image5"></div>
+      </div>
+    </div>
+  );
+};
+
+export default MainImages;
---
+++ src/Components/Main/MainText.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainText = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MainText
