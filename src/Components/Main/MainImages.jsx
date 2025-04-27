5defe6585071bf14797268a3c6de5bde171f443d
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-04-27 19:10:49 +0500

    AI-Automation

--- src/Components/Main/MainImages.jsx
+++ src/Components/Main/MainImages.jsx
@@ -5,19 +5,19 @@ const MainImages = () => {
     <div className="flex flex-wrap justify-center gap-5">
       {/* Left Column */}
       <div className="flex flex-col gap-3">
-        <div className="w-60 h-60 lg:w-56 lg:h-56  md:w-48 md:h-48 bg-gray-300 rounded-2xl" />
-        <div className="w-52 h-52 lg:w-44 lg:h-44 md:w-38 md:h-38 bg-gray-300 rounded-2xl" />
+        <div className="w-30 h-30 lg:w-56 lg:h-56  md:w-48 md:h-48 bg-gray-300 rounded-2xl" />
+        <div className="w-25 h-25 lg:w-44 lg:h-44 md:w-38 md:h-38 bg-gray-300 rounded-2xl" />
       </div>
 
       {/* Middle Column */}
       <div className="flex flex-col gap-3">
-        <div className="w-52 h-52 lg:w-44 lg:h-44 md:w-38 md:h-38 bg-gray-300 rounded-2xl" />
-        <div className="w-44 h-44 lg:w-36 lg:h-36 md:w-28 md:h-28 bg-gray-300 rounded-2xl" />
+        <div className="w-25 h-25 lg:w-44 lg:h-44 md:w-38 md:h-38 bg-gray-300 rounded-2xl" />
+        <div className="w-20 h-20 lg:w-36 lg:h-36 md:w-28 md:h-28 bg-gray-300 rounded-2xl" />
       </div>
 
       {/* Right Column */}
       <div className="flex flex-col gap-3">
-        <div className="w-44 h-44 lg:w-36 lg:h-36 md:w-28 md:h-28 bg-gray-300 rounded-2xl mt-10 md:mt-20" />
+        <div className="w-20 h-20 lg:w-36 lg:h-36 md:w-28 md:h-28 bg-gray-300 rounded-2xl mt-10 md:mt-20" />
       </div>
     </div>
   );
