e33b2225228f0a4ada1c6cd9e240e2cfe499276c
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-04-29 14:31:18 +0500

    AI-Automation

--- src/Components/About/AboutTestimonials.jsx
+++ src/Components/About/AboutTestimonials.jsx
@@ -23,6 +23,21 @@ const AboutTestimonials = () => {
         allowTouchMove={false} // disable dragging with mouse/touch
       >
         <SwiperSlide>
+          <div>
+            <div>
+              <div className="slide-image">
+                <img src="https://via.placeholder.com/600x300" alt="Slide 1" />
+              </div>
+              <div className="slide-name">
+                <h4>Zara Kline</h4>
+                <p>Sales Director</p>
+              </div>
+            </div>
+            <div className="data">
+              <div className="line"></div>
+              <p>The Setup Wizard transformed our sales process!</p>
+            </div>
+          </div>
           <img src="https://via.placeholder.com/600x300" alt="Slide 1" />
         </SwiperSlide>
         <SwiperSlide>
